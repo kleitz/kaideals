5dd95c7ab50759b549e3329c3771a7e2545e766e
Patrick McCaffrey <patrick.mccaffrey.19@gmail.com>
2017-11-19 21:19:45 +1300

    changed webpack to non inject script tags, added a contributors component

--- containers/app/src/app/components/Intro.jsx
+++ containers/app/src/app/components/Intro.jsx
@@ -4,12 +4,15 @@ import Header from './Header.jsx';
 import DealsContainer from './deals/deal_container.jsx';
 import Footer from './Footer.jsx';
 
+import ContributorContainer from './coders/contributor_container.jsx'
+
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header />
-        <DealsContainer />
+        <DealsContainer/>
+        <ContributorContainer />
         <Footer />
       </div>
     );
